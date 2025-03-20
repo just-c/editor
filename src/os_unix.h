993251d983e9d7e34c55d2f47a080661261c2015
Elias Pekkala <eliaspekkala98@gmail.com>
2025-03-20 15:13:13 +0100

    remove define for non linux platforms

--- src/os_unix.h
+++ src/os_unix.h
@@ -10,14 +10,8 @@
 #define CONF_DIR ".config/nino"
 #define DIR_SEP "/"
 
-#ifdef __linux__
-// Linux
 #include <linux/limits.h>
 #define EDITOR_PATH_MAX PATH_MAX
-#else
-// Other
-#define EDITOR_PATH_MAX 4096
-#endif
 
 struct FileInfo {
   struct stat info;
